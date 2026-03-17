67342a4e74dc3aa9fc06cadac9e6f52cc1960982
Pierre-Louis Wasmer <pierre-louis.wasmer@cea.fr>
2026-03-17 16:25:03 +0100

    undid scale_nH match to main ramses- makes barotrop simulation diverge

--- patch/collapse/units.f90
+++ patch/collapse/units.f90
@@ -49,8 +49,12 @@ subroutine units(scale_l,scale_t,scale_d,scale_v,scale_nH,scale_T2)
   !scale_T2 = mu_gas**2 * mH**2 * pc**2 * Grav / kb
   scale_T2 = mH/kB * scale_v**2 * mu_gas !this extra mu gas is used to match prev def of scale_T2
 
-  scale_nH = X/(mH) * scale_d !some functions in frig assume scale_nH =1
- 
+  !scale_nH = X/(mH) * scale_d !some functions in frig assume scale_nH =1
+  if(rt_protostar_m1) then
+     scale_nH = X/(mH) * scale_d !1.0_dp
+  else
+     scale_nH = 1.0_dp
+  endif
 
   scale_kappa = 1.0_dp / scale_l
 
